c8ee1687ae2e3484ce4f186c263743122e94cafc
Konstantin Zhdanov <andreewwhite@gmail.com>
2021-01-11 11:23:32 +0700

    test

--- tasks/build-img.js
+++
@@ -1,8 +0,0 @@
-const { gulp, rename, dirs } = require('../variables.js')
-
-const buildImg = () => {
-  return gulp.src(dirs.src.img)
-  .pipe(gulp.dest(dirs.dist.img))
-}
-
-exports.buildImg = buildImg
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-// vue.config.js
-module.exports = {
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    writeToDisk: true,
-    hot: false
-  }
-}
